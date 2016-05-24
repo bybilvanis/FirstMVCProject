d533109db856ee60c5e9dccc315e204f55f40bf7
Osmanlı Genç <Osmanl Gen>
2016-05-24 12:08:23 +0200

    verzenden van gegevens

--- controllers/admin/editor.php
+++ controllers/admin/editor.php
@@ -1,5 +1,20 @@
 <?php
 // model
+include_once 'models/Blog_Entry_Table.class.php';
+$entryTable= new Blog_Entry_Table($db);
+$editorSubmitted= isset($_POST['action']);
+if ($editorSubmitted){
+    $buttonClicked = $_POST['action'];
+    //was "save" button clicked
+    $insertNewEntry = ($buttonClicked === 'Save');
+    if ($insertNewEntry) {
+        //get title and entry data from editor form
+        $title = $_POST['title'];
+        $entry = $_POST['entry'];
+        //save the new entry
+        $entryTable->saveEntry($title, $entry);
+    }
+}
 
 // view
 $editorOutput = include_once 'views/admin/editor-html.php';
--- views/admin/editor-html.php
+++ views/admin/editor-html.php
@@ -1,12 +1,12 @@
 <?php
 return "
-<form action='admin.php?page=editor' id='editor'>
+<form action='admin.php?page=editor' id='editor' method='post'>
     <fieldset>
      <legend>New Entry Submission</legend>
      <label for='title'>Title</label>
      <input type='text' name='title' maxlength='150'>
      <label for='entry'>Entry</label>
-     <textarea name='enrty'></textarea>
+     <textarea name='entry'></textarea>
      <fieldset id='editor-buttons'>
         <input type='submit' name='action' value='Save'>
         <input type='submit' name='action' value='Delete'>
